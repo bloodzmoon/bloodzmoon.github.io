98d163ada15f6697abf1da533eb56de361c287d4
Thanyathon <61010497@kmitl.ac.th>
2021-01-20 15:25:23 +0700

    🧹 Clean repo

--- src/components/Repo/index.js
+++
@@ -1,8 +0,0 @@
-import Profile from './Profile'
-import Detail from './Detail'
-import Search from './Search'
-import RepoArea from './RepoArea'
-import StatBar from './StatBar.vue'
-import StatCircle from './StatCircle.vue'
-
-export { Profile, Detail, Search, RepoArea, StatBar, StatCircle }
